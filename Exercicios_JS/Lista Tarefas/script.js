651c02b0e0c8b580d8b97e92bcab713a7da7a91d
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-03-20 16:15:45 -0300

    .

--- Exercicios_JS/Lista Tarefas/script.js
+++ Exercicios_JS/Lista Tarefas/script.js
@@ -1,1 +1,37 @@
-document.getElementById('botao')
+// class bd salva as atividades no local storage
+class Bd {
+    constructor (){
+        this.id = id
+    }
+    salvar(id){
+        localStorage.setItem(id) + 1
+    }
+
+}
+
+// objeto do banco de dados simples salvo no local storage
+let bd = new Bd()
+
+// class tarefa a qual irá receber os dados - eu acho
+class Tarefa {
+    constructor () {
+
+    }
+}
+
+// objeto da classe tarefa
+let tarefa = new Tarefa()
+
+// função a qual vai inserir os dados e salvar no localstorage
+function inserirAtividades (){
+    let titulo = document.getElementById('titulo').value
+    let descricao = document.getElementById('descricao').value
+    bd.salvar()
+}
+
+// função a qual vai listar as atividades em forma de lista estilizada como um input
+function mostrarAtividades(){
+    alert('função acionada')
+}
+
+
