0864df522f0df2c2ef43023ba487448ff5692b93
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-06 17:31:08 -0300

    pequenas correções no entendimento do codigo
    corrigindo importações dinamicas.

--- Novas_atualizacoes_ES/import/lib.mjs
+++ Novas_atualizacoes_ES/import/lib.mjs
@@ -3,7 +3,3 @@ export const saudacao = () => {
     return `Olá, seja muito bem-vindo!`
 }
 
-export function getEnderecoByCEP(cep) {
-    return fetch(`https://viacep.com.br/ws/${cep}/json/`) //api de cep 
-        .then(response => response.json())   
-}
