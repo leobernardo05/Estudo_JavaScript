94634181998bdd8b960df4b62517d620dd6aaeb9
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-09 14:28:44 -0300

    .
    Entendendo a await atualizado do ES13

---
+++ Novas_atualizacoes_ES/module.js
@@ -0,0 +1,34 @@
+console.log('Módulo importado');
+
+export async function fetchDados (){
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('Executou')
+        }, 3000)
+    })
+}
+
+const dados = await fetchDados() //aguarda a resolução da promise
+console.log(dados)
+
+console.log('fim do processamento')
+
+/*export async function aguardarPromessa() {
+    consnole.log('inicio do processamento')
+
+    function fetchDados(){
+        return new Promise(resolve => {
+            setTimeout(()=> {
+                resolve('Executou')
+            },3000)
+        })
+    }
+    //aguardamos pelo processamento
+    //no contexto da função
+    const dados = await fetchDados() 
+    console.log(dados)
+
+    console.log('fim do processamento')
+}
+
+aguardarPromessa()*/
